6d1e7f23e8fdbe825677bcf6a2b38563ce115335
Ray Mitchell <raymonxo@gmail.com>
2016-08-16 00:11:20 -0700

    Update FibonacciSequenceTests.scala

--- project-euler/src/test/scala/raymitchell/euler/util/FibonacciSequenceTests.scala
+++ project-euler/src/test/scala/raymitchell/euler/util/FibonacciSequenceTests.scala
@@ -5,7 +5,6 @@ import org.scalatest.{FlatSpec, Matchers}
 class FibonacciSequenceTests extends FlatSpec with Matchers {
 
   "Fibonacci Sequence" should "start with the first 7 Fibonacci numbers" in {
-
     FibonacciSequence.take(7).toList should be (List(1, 1, 2, 3, 5, 8, 13))
   }
 }
