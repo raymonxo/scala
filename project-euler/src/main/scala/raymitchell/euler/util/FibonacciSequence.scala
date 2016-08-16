410eec6fbcef636e3b6813568ce2722d2b5c5ce6
Ray Mitchell <raymonxo@gmail.com>
2016-08-16 00:09:53 -0700

    Update FibonacciSequence.scala

--- project-euler/src/main/scala/raymitchell/euler/util/FibonacciSequence.scala
+++ project-euler/src/main/scala/raymitchell/euler/util/FibonacciSequence.scala
@@ -6,14 +6,12 @@ package raymitchell.euler.util
 object FibonacciSequence extends Iterable[Int] {
 
   override def iterator: Iterator[Int] = new Iterator[Int] {
-
     var a = 1
     var b = 1
 
     override def hasNext: Boolean = true
 
     override def next(): Int = {
-
       val result = a
       a = b
       b = result + b
