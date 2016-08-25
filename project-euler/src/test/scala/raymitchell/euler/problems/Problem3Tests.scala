a5a2531d5f6b0a1502cc29cfde5e7551d81f9cfd
Ray Mitchell <raymonxo@gmail.com>
2016-08-25 12:14:02 -0700

    Update Problem3Tests.scala

--- project-euler/src/test/scala/raymitchell/euler/problems/Problem3Tests.scala
+++ project-euler/src/test/scala/raymitchell/euler/problems/Problem3Tests.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Problem3Tests extends FlatSpec with Matchers {
 
-  "Problem 3" should "should be solved correctly" in {
+  "Problem 3" should "be solved correctly" in {
     Problem3.solve() should be (6857l)
   }
 }
