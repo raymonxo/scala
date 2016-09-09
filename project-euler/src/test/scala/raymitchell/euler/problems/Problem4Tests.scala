1b9637c8580a15f143fc422f51eff652aa72a628
Ray Mitchell <raymonxo@gmail.com>
2016-09-09 13:40:37 -0700

    no message

--- project-euler/src/test/scala/raymitchell/euler/problems/Problem4Tests.scala
+++ project-euler/src/test/scala/raymitchell/euler/problems/Problem4Tests.scala
@@ -5,6 +5,6 @@ import org.scalatest.{FlatSpec, Matchers}
 class Problem4Tests extends FlatSpec with Matchers {
 
   "Problem 4" should "be solved correctly" in {
-    Problem4.solve() should be (906609)
+    Problem4.solve should be (906609)
   }
 }
