0ebc35ea4860156568b509e4de0a561885f17900
Ray Mitchell <rmitchell@fairwaytech.com>
2016-09-28 16:43:41 -0700

    Solve problem 13.

---
+++ project-euler/src/test/scala/raymitchell/euler/problems/Problem13Tests.scala
@@ -0,0 +1,10 @@
+package raymitchell.euler.problems
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class Problem13Tests extends FlatSpec with Matchers {
+
+  "Problem 13" should "be solved correctly" in {
+    Problem13.solve should be (5537376230L)
+  }
+}
